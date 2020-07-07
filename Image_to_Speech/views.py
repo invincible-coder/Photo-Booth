67e2dc72513a860b3183b605479b0ac9881ed2b2
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-07-07 11:06:19 +0530

    validation of file types still left

--- Image_to_Speech/views.py
+++ Image_to_Speech/views.py
@@ -6,3 +6,9 @@ def HomeView(request):
         pass
     else:
         return render(request,'home.html')
+def Image_to_Text_View(request):
+    return render(request,'image_to_text.html')
+def Image_to_Text_process_View(request):
+    return render(request, 'home.html')
+def Text_to_speech_View(request):
+    pass
