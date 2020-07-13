e9db32315e0e73c66beb71cdeecc3234b4095186
m-e-l-u-h-a-n <pururshottam.tiwari.cd.cse19@itbhu.ac.in>
2020-07-13 23:49:44 +0530

    implemented file conversion and download method

--- Image_to_Speech/urls.py
+++ Image_to_Speech/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path,re_path
-from .views import HomeView,Image_to_Text_View,Text_to_speech_View,Image_to_Text_process_View
+from .views import HomeView,Image_to_Text_View,Text_to_speech_View,clearFilesView,downloadFile
 urlpatterns = [
     re_path(r'^$',HomeView,name='home'),
     re_path(r'^image_to_text/$',Image_to_Text_View,name="image_to_text"),
     re_path(r'^text_to_speech/$',Image_to_Text_View,name="text_to_speech"),
-    re_path(r'^image_to_text_process/$',Image_to_Text_process_View,name = 'image_to_text_process'),
+    re_path(r'^clearFiles/$',clearFilesView,name = 'clearFiles'),
+    path('Photo_Booth/static/results/<str:filename>',downloadFile,name = 'downloadFile')
 ]
